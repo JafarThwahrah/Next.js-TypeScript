89448a341e5f64c9a71f626c000bc7f700cc97e6
JafarThwahrah <jaffardawahreh2@gmail.com>
2023-02-25 17:49:55 +0300

    getStaticPath fallback true

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,37 +1,54 @@
 import { post } from "models/post";
+import { useRouter } from "next/router";
+const PostDetails: React.FC<{ post: post }> = ({ post }) => {
+  const router = useRouter();
 
-const PostDetails: React.FC<{ post: post}> = ({ post }) => {
+  if (router.isFallback) {
+    return <h1>Loading</h1>;
+  }
   return (
     <>
       <h1>Posts Details</h1>
 
-            <h3>
-              {post.id}-{post.title}
-            </h3>
-            <h3>{post.userId}</h3>
-            <h3>{post.body}</h3>
-        
+      <h3>
+        {post.id}-{post.title}
+      </h3>
+      <h3>{post.userId}</h3>
+      <h3>{post.body}</h3>
     </>
   );
 };
 
 export async function getStaticPaths() {
+  // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const data = await response.json();
 
-    return {
-        paths : [
-            {
-                params : {postId : '1'}
-            },
-            {
-                params : {postId : '2'}
-            }, {
-                params : {postId : '3'}
-            },
-            
-        ],
-        fallback : false
+  // const paths = data.map((post: post) => {
+  //   return {
+  //     params: {
+  //       postId: `${post.id}`,
+  //     },
+  //   };
+  // });
+  // return {
+  //   paths,
+  //   fallback: false,
+  // };
 
-    }
+  return {
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: true,
+  };
 }
 
 export async function getStaticProps(context: { params: any }) {
@@ -41,6 +58,11 @@ export async function getStaticProps(context: { params: any }) {
   );
   const data = await response.json();
 
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
       post: data,
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,33 +1,34 @@
-import { post } from "models/post"
-import Link from "next/link"
-const PostsList : React.FC<{posts:Array<post>}> = ({posts})=>{
-return <>
-<h1>Posts List</h1>
+import { post } from "models/post";
+import Link from "next/link";
+const PostsList: React.FC<{ posts: Array<post> }> = ({ posts }) => {
+  return (
+    <>
+      <h1>Posts List</h1>
 
-{posts.map((post)=>{
-    return (
-        <div key={post.id}>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
             <Link href={`posts/${post.id}`} passHref>
-            <h3>{post.id}-{post.title}</h3>
+              <h3>
+                {post.id}-{post.title}
+              </h3>
             </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-        </div>
-    )
-})}
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
 
-</>
+  return {
+    props: {
+      posts: data.slice(0, 3),
+    },
+  };
 }
 
-export async function getStaticProps(){
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await response.json()
-
-    return {
-        props: {
-            posts : data.slice(0,3)
-        }
-    }
-}
-
-
-export default PostsList
+export default PostsList;
